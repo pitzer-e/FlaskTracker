645faf19e51ad937fcf6d6c9f491760eb72fe3ce
Ethan Pitzer <ethan.pitzer@gmail.com>
2021-05-19 13:08:12 -0700

    Documentation for existing templates and creation of report.html

--- app.py
+++ app.py
@@ -9,6 +9,11 @@ def home_page():
     return render_template('home.html')
 
 
-@app.route('/data')
-def data_page():
-    return render_template('data.html', item_name="Place")
+@app.route('/location')
+def location_page():
+    return render_template('location.html', item_name="Place")
+
+
+@app.route('/report')
+def report_page():
+    return render_template('report.html', item_name="Place")
