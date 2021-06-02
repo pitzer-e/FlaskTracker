9245f140674ac54d5ac0b993c30c0b16b754f6c2
Ethan Pitzer <ethan.pitzer@gmail.com>
2021-06-02 10:18:12 -0700

    2021-06-02, Forms Linked to DB

--- tracker/routes.py
+++ tracker/routes.py
@@ -1,7 +1,8 @@
 from tracker import app
-from flask import render_template
+from flask import render_template, redirect, url_for
 from tracker.models import User
 from tracker.forms import RegisterForm
+from tracker import db
 
 
 @app.route('/')
@@ -25,7 +26,18 @@ def admin_page():
     users = User.query.all()
     return render_template('admin.html', users=users)
 
-@app.route('/register')
+
+@app.route('/register', methods=['GET', 'POST'])
 def register_page():
     form = RegisterForm()
+    if form.validate_on_submit():
+        user_to_create = User(user_name=form.user_name.data,
+                              first_name=form.first_name.data,
+                              last_name=form.last_name.data,
+                              password_hash=form.password_1.data)
+
+        db.session.add(user_to_create)
+        db.session.commit()
+        return redirect(url_for('home_page'))
+
     return render_template('register.html', form=form)
