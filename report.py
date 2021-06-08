564ca4409d7ba60ea45358ee52f7a79473cc5006
danthefisherman <dthughes@eou.edu>
2021-06-08 12:41:38 -0700

    report.py - update_infect_chance

--- report.py
+++ report.py
@@ -1,16 +1,20 @@
-from graph import Graph
+from tracker.graph import Graph
 from tracker import db
 from tracker.models import *
 import datetime
 from datetime import time, date
 
-def update_infect_chance():
 
-    return 0
+def update_infect_chance():
+    records = db.session.query(Location).order_by(Location.date).all()
+    for record in records:
+        print(f"{record.name}: {record.date}")
+
 
 def gen_report(submitted_location):
     print(f"checking {submitted_location.name}")
     graph = Graph()
+    update_infect_chance()
     # pass from date from report page
     python_date = submitted_location.date
     python_time = submitted_location.time
@@ -18,11 +22,12 @@ def gen_report(submitted_location):
     for record in records:
         if record.owner != submitted_location.owner and record.time < python_time:
             print(f"checking against {record.name} from {User.query.filter_by(id=record.owner).first().username}")
-            if User.query.filter_by(id=record.owner).first().is_infected or User.query.filter_by(id=record.owner).first().infection_chance > 0:
+            if User.query.filter_by(id=record.owner).first().is_infected or User.query.filter_by(
+                    id=record.owner).first().infection_chance > 0:
                 print(f"Connecting {record.id} to {submitted_location.id}")
                 # add edge between this location and passed location
                 graph.add_edge(record.id, submitted_location.id)
                 # if record owner is infected
-                    # then that is the shortest path
+                # then that is the shortest path
                 # elif
-                    # find out who may have infected record owner
+                # find out who may have infected record owner
--- tracker/routes.py
+++ tracker/routes.py
@@ -6,6 +6,7 @@ from tracker import db
 from flask_login import login_user, logout_user, login_required, current_user
 from datetime import date, time
 from tracker.graph import Graph
+import report
 
 
 @app.route('/')
@@ -54,6 +55,10 @@ def report_page():
             graph = Graph()
             entered_date = request.form.get('Date')
             infected_user = User.query.filter_by(is_infected=True).first()
+
+            date_to_check = request.form.get('Date')
+            location_to_check = Location.query.filter_by(date=date_to_check, owner=current_user.id).first()
+            report.gen_report(location_to_check)
 
             flash(f'User ID of infected user: {infected_user.id}', category='info')
 
